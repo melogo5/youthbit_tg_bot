5350b5fea3ae691c958d8ef215acb65cf8360471
melogo5 <Evedoz572-Utl007iii>
2023-02-18 12:29:31 +0500

    bot init

---
+++ server/index.ts
@@ -0,0 +1,44 @@
+import { Telegraf } from 'telegraf';
+
+const token = '5969288783:AAGsRdbhsqrgBkfv6GwAezpva1OJq3AnjcY';
+
+const bot = new Telegraf(token);
+
+bot.command('quit', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.leaveChat(ctx.message.chat.id);
+
+  // Using context shortcut
+  await ctx.leaveChat();
+});
+
+bot.on('text', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+
+  // Using context shortcut
+  await ctx.reply(`Hello ${ctx.state.role}`);
+});
+
+bot.on('callback_query', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
+
+  // Using context shortcut
+  await ctx.answerCbQuery();
+});
+
+bot.on('inline_query', async (ctx) => {
+  const result = [];
+  // Explicit usage
+  await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result);
+
+  // Using context shortcut
+  await ctx.answerInlineQuery(result);
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
